8460adc9d5e156259061d61e4257d7312afa3c26
Usuario <Usuario@DESKTOP-8O70O2F>
2022-07-31 19:11:56 -0300

    server cloud

--- src/main/java/com/portfolio/portfolio/PortfolioApplication.java
+++ src/main/java/com/portfolio/portfolio/PortfolioApplication.java
@@ -2,6 +2,10 @@ package com.portfolio.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class PortfolioApplication {
@@ -10,6 +14,15 @@ public class PortfolioApplication {
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
         
-  
+   @Bean
+   public WebMvcConfigurer corsConfigurer() {
+      return new WebMvcConfigurerAdapter() {
+         @Override
+         public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/portfolio").allowedOrigins("http://localhost:8080");
+         }
+      };
+   }
+        
 
 }
